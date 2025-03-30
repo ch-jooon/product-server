4d08ff1743bd2d34f80648414bff1daf760fcd08
ch-jooon <ch.jooon2@gmail.com>
2025-03-31 02:45:49 +0900

    feat: 도메인 repository JPA 구현체 추가

---
+++ product-infra/product-infra-jpa/src/main/kotlin/musinsa/product/infra/jpa/config/JpaConfig.kt
@@ -0,0 +1,10 @@
+package musinsa.product.infra.jpa.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = ["musinsa.product.infra.jpa"])
+class JpaConfig
